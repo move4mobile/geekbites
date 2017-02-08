a3c9a1921ba592de1a34cf55dc1c6c250850aada
cassshh <casvd@hotmail.com>
2017-02-08 23:57:03 +0100

    Re-use code deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
+deploy () {
+    echo "Initiate deployment on $1 environment"
+    firebase use "$1" --token "${FIREBASE_API_TOKEN}"
+    firebase deploy --token "${FIREBASE_API_TOKEN}"
+}
+
+check_on_pr () {
+    echo "Checking on PR..."
+    echo "Pull Request: $TRAVIS_PULL_REQUEST"
+    if [ $TRAVIS_PULL_REQUEST == false ]; then
+        deploy "$1"
+    else
+        echo "Is a pull request..."
+        echo "Not deploying."
+    fi
+}
+
 echo "Executing deploy script..."
 echo "Branch: $TRAVIS_BRANCH"
 
 if [ $TRAVIS_BRANCH == "master" ]; then
     echo "Branch is master..."
-    echo "Checking on PR..."
-    echo "Pull Request: $TRAVIS_PULL_REQUEST"
-    if [ $TRAVIS_PULL_REQUEST == false ]; then
-        echo "Initiate deployment :)"
-        firebase use "${FIREBASE_PROJECT_ID_PROD}" --token "${FIREBASE_API_TOKEN}"
-        firebase deploy --token "${FIREBASE_API_TOKEN}"
-    else
-        echo "Is a pull request..."
-        echo "Not deploying."
-    fi
+    check_on_pr "${FIREBASE_PROJECT_ID_PROD}"
 elif [ $TRAVIS_BRANCH == "develop" ]; then
     echo "Branch is develop..."
-    echo "Checking on PR..."
-    echo "Pull Request: $TRAVIS_PULL_REQUEST"
-    if [ $TRAVIS_PULL_REQUEST == false ]; then
-        echo "Initiate deployment :)"
-        firebase use "${FIREBASE_PROJECT_ID_DEV}" --token "${FIREBASE_API_TOKEN}"
-        firebase deploy --token "${FIREBASE_API_TOKEN}"
-    else
-        echo "Is a pull request..."
-        echo "Not deploying."
-    fi
+    check_on_pr "${FIREBASE_PROJECT_ID_DEV}"
 else
     echo "Not develop branch or master..."
     echo "Not deploying."
