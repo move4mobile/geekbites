0e1402fd9e1a9548a058753b147bfb81c9d3caf4
cassshh <casvd@hotmail.com>
2017-02-08 23:44:00 +0100

    Cleanup deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,23 +14,19 @@ if [ $TRAVIS_BRANCH == "master" ]; then
         echo "Is a pull request..."
         echo "Not deploying."
     fi
-else
-    if [ $TRAVIS_BRANCH == "develop" ]; then
-        echo "Branch is develop..."
-        echo "Checking on PR..."
-        echo "Pull Request: $TRAVIS_PULL_REQUEST"
-        if [ $TRAVIS_PULL_REQUEST == false ]; then
-            echo "Initiate deployment :)"
-            firebase use "${FIREBASE_PROJECT_ID_DEV}" --token "${FIREBASE_API_TOKEN}"
-            firebase deploy --token "${FIREBASE_API_TOKEN}"
-        else
-            echo "Is a pull request..."
-            echo "Not deploying."
-        fi
+elif [ $TRAVIS_BRANCH == "develop" ]; then
+    echo "Branch is develop..."
+    echo "Checking on PR..."
+    echo "Pull Request: $TRAVIS_PULL_REQUEST"
+    if [ $TRAVIS_PULL_REQUEST == false ]; then
+        echo "Initiate deployment :)"
+        firebase use "${FIREBASE_PROJECT_ID_DEV}" --token "${FIREBASE_API_TOKEN}"
+        firebase deploy --token "${FIREBASE_API_TOKEN}"
     else
-        echo "Not develop branch or master..."
+        echo "Is a pull request..."
         echo "Not deploying."
     fi
+else
     echo "Not develop branch or master..."
     echo "Not deploying."
 fi
