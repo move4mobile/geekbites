30486e5c34721e215372c21a227ea7aad44f9395
Michael Schilling <mschilling@move4mobile.com>
2020-01-09 14:05:48 +0100

    Move assets base folder + move sass files

--- .eleventy.js
+++ .eleventy.js
@@ -9,7 +9,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addCollection('tagList', require('./_11ty/getTagList'));
 
-  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('src/assets');
 
 //   eleventyConfig.addPassthroughCopy('src/css');
 
