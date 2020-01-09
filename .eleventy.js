8168e4bafd36a6e2d641845cff5a7a1b24faf892
Michael Schilling <mschilling@move4mobile.com>
2020-01-09 10:25:11 +0100

    Initial eleventy config file

---
+++ .eleventy.js
@@ -0,0 +1,43 @@
+const { DateTime } = require('luxon');
+
+module.exports = function(eleventyConfig) {
+//   eleventyConfig.setDataDeepMerge(true);
+
+//   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+
+//   eleventyConfig.addCollection('tagList', require('./_11ty/getTagList'));
+
+//   eleventyConfig.addPassthroughCopy('src/css');
+
+  // Filters
+  eleventyConfig.addFilter('readableDate', dateObj => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'dd LLL yyyy'
+    );
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', dateObj => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  // You can return your Config object (optional).
+  return {
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'dist'
+    }
+  };
+};
